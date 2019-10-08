43979ce043d10c50d0eb0daa9d3520fd432b8578
root <root@node002.paas-test.eubrmb.com>
2019-10-08 15:22:09 +0800

    a

--- amp-ub-config-smtp.sh
+++ amp-ub-config-smtp.sh
@@ -5,7 +5,7 @@ oc project $AMP_PROJECT_NAME
 oc patch configmap smtp -p '{"data":{"address":"universalbeijing-com.mail.protection.partner.outlook.cn"}}'
 #oc patch configmap smtp -p '{"data":{"username":""}}'
 #oc patch configmap smtp -p '{"data":{"password":""}}'
-oc patch configmap smtp -p '{"data":{"port":25}}'
+oc patch configmap smtp -p '{"data":{"port":"25"}}'
 
 oc rollout latest dc/system-app
 oc rollout latest dc/system-sidekiq
